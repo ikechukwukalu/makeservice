0e294708a50f3bd751b00f63983b6ebe07e77d05
Monitecture <100952206+monitecture@users.noreply.github.com>
2023-05-02 00:10:23 +0100

    Added new tests

--- Tests/CommandTest.php
+++ Tests/CommandTest.php
@@ -17,5 +17,19 @@ class CommandTest extends TestCase
         $this->artisan('make:request SampleRequest')->assertSuccessful();
 
         $this->artisan('make:service SampleService --request=SampleRequest --force')->assertSuccessful();
+
+        $this->artisan('make:trait SampleTrait')->assertSuccessful();
+
+        $this->artisan('make:trait SampleTrait --force')->assertSuccessful();
+
+        $this->artisan('make:interface SampleInterface')->assertSuccessful();
+
+        $this->artisan('make:interface SampleInterface --force')->assertSuccessful();
+
+        $this->artisan('make:interface UserRepositoryInterface --model=User')->assertSuccessful();
+
+        $this->artisan('make:repository UserRepository --interface=UserRepositoryInterface --model=User')->assertSuccessful();
+
+        $this->artisan('make:repository UserRepository --interface=UserRepositoryInterface --model=User --force')->assertSuccessful();
     }
 }
